fbcca9a4961f84e1b591cd195091ba4c50f7044d
xunxiancoming <1475711781@qq.com>
2020-09-14 16:13:39 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import aixos from 'axios'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
